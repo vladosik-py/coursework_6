da478645f78ee2eaa52b2ffc747f87fec8e395f2
vladosik_py <vbsmulskaya@gmail.com>
2023-05-16 17:14:46 +0300

    cw6

--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -1,8 +1,7 @@
-from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
-from users.managers import UserManager
 from phonenumber_field.modelfields import PhoneNumberField
-
+from users.managers import UserManager
 from users.managers import UserRoles
 
 
